f8120752908832f05d72aafc84322bd6f3f8dd4c
Kevin-De <kevinde@hotmail.co.uk>
2017-05-28 19:36:47 +0100

    added a date coverter for room (may not work)

---
+++ app/src/main/java/com/example/kleog/fitnessapp/DateConverter.java
@@ -0,0 +1,31 @@
+package com.example.kleog.fitnessapp;
+import android.arch.persistence.room.TypeConverter;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Kevin on 28/05/2017.
+ */
+
+class DateConverter {
+
+    @TypeConverter
+    public static Date toDate(String strDate) {
+        try {
+            return strDate == null ? null :  new SimpleDateFormat("yyyyMMddHHmmss").parse(strDate + "000000"); //takes a string date from SQLite and converts it to Date in format
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Log.d("DATE_CONVERTER", "error converting: " + strDate + " , to a Date data type");
+        return null;
+    }
+
+    @TypeConverter
+    public static String toDateString (Date date) {
+        Log.d("DATE_CONVERTER", "toDateString: " + date + " becomes: " + new SimpleDateFormat("dd/MM/YYYY").format(new Date()).toString());
+        return date == null ? null :  new SimpleDateFormat("dd/MM/YYYY").format(new Date()).toString();
+    }
+}
