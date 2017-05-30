c809eb2184acb3290de3e34299715a86a1ac5751
Kevin-De <kevinde@hotmail.co.uk>
2017-05-30 15:25:32 +0100

    attempting to insert and query data from the db using AyncTask

--- app/src/main/java/com/example/kleog/fitnessapp/MealActivity.java
+++ app/src/main/java/com/example/kleog/fitnessapp/MealActivity.java
@@ -1,17 +1,23 @@
 package com.example.kleog.fitnessapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.kleog.fitnessapp.UserNutritionDatabase.DailyUserInfoModel;
+import com.example.kleog.fitnessapp.UserNutritionDatabase.UserNutritionDB;
+
+import java.util.Date;
+
 /**
  * Created by Kevin on 23/05/2017.
  */
 
 public class MealActivity extends AppCompatActivity {
-
+    public UserNutritionDB db;
     public String mealType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,43 @@ public class MealActivity extends AppCompatActivity {
 
         setTitle(mealType);
 
+        db = UserNutritionDB.getDatabase(this.getApplicationContext());
+
     }
 
     public void onClickNotFinished(View view){
-        Toast.makeText(this, "bitch", Toast.LENGTH_LONG).show();
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                db.DailyUserInfoModel().insert(new DailyUserInfoModel(new Date(), 150, 100, 75, 50, 73));
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void param) {
+                Toast.makeText(getApplicationContext(), "user information was inserted", Toast.LENGTH_LONG).show();
+                String info = "user info not found";
+                new AsyncTask<String, Void, Void>(){
+                    @Override
+                    protected Void doInBackground(String... params){
+                        params[0] = db.DailyUserInfoModel().getDate(new Date()).toString();
+                        return null;
+                    }
+                }.execute(info);
+                Toast.makeText(getApplicationContext(), info, Toast.LENGTH_LONG).show();
+
+
+            }
+        }.execute();
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+
 
 }
--- app/src/main/java/com/example/kleog/fitnessapp/UserNutritionDatabase/UserNutritionDB.java
+++ app/src/main/java/com/example/kleog/fitnessapp/UserNutritionDatabase/UserNutritionDB.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
 
 /**
  * Created by Kevin on 29/05/2017.
