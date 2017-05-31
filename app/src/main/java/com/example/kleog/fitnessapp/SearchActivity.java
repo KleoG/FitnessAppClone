07502ec84c5f0476138b1079a3f49afc6fca082d
KleoG <kleogeorgiou@hotmail.com>
2017-05-31 20:11:40 +0100

    complete and functional search functionality picking food items

--- app/src/main/java/com/example/kleog/fitnessapp/SearchActivity.java
+++ app/src/main/java/com/example/kleog/fitnessapp/SearchActivity.java
@@ -2,8 +2,12 @@ package com.example.kleog.fitnessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,4 +39,31 @@ public class SearchActivity extends AppCompatActivity {
 
         lv.setAdapter(adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem item = menu.findItem(R.id.menuSearch);
+
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return true;
+    }
+
 }
