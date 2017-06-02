0b67b242ca1bb5c0c6903835c7ab710a06c96d3e
Kevin-De <kevinde@hotmail.co.uk>
2017-06-02 22:02:25 +0100

    added some fat secret testing to the search activity (do not use yet)

--- app/src/main/java/com/example/kleog/fitnessapp/SearchActivity.java
+++ app/src/main/java/com/example/kleog/fitnessapp/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.kleog.fitnessapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,14 +13,30 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.fatsecret.platform.model.CompactFood;
+import com.fatsecret.platform.model.CompactRecipe;
+import com.fatsecret.platform.model.Food;
+import com.fatsecret.platform.model.Recipe;
+import com.fatsecret.platform.services.Response;
+import com.fatsecret.platform.services.android.Request;
+import com.fatsecret.platform.services.android.ResponseListener;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> adapter;
 
     private String mealType;
+
+    //api
+    String key;
+    String secret;
+    String query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +98,73 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+
+
+        //fat secret API stuff
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        Listener listener = new Listener();
+
+        Request req = new Request(key, secret, listener);
+
+
+        //This response contains the list of food items at zeroth page for your query
+        req.getFoods(requestQueue, query, 0);
+
+        //This response contains the list of food items at page number 3 for your query
+        //If total results are less, then this response will have empty list of the food items
+        req.getFoods(requestQueue, query, 3);
+
+        //This food object contains detailed information about the food item
+        req.getFood(requestQueue, 29304L);
+
+        //This response contains the list of recipe items at zeroth page for your query
+        req.getRecipes(requestQueue, query, 0);
+
+        //This response contains the list of recipe items at page number 2 for your query
+        //If total results are less, then this response will have empty list of the recipe items
+        req.getRecipes(requestQueue, query, 2);
+
+        //This recipe object contains detailed information about the recipe item
+        req.getRecipe(requestQueue, 315L);
     }
 
+
+
 }
+class Listener implements ResponseListener {
+    @Override
+    public void onFoodListRespone(Response<CompactFood> response) {
+        Log.d("FAT_SECRET", "onFoodListRespone: TOTAL FOOD ITEMS: " + response.getTotalResults());
+
+        List<CompactFood> foods = response.getResults();
+        //This list contains summary information about the food items
+
+        Log.d("FAT_SECRET", "onFoodListRespone: =========FOODS============");
+        for (CompactFood food: foods) {
+            System.out.println(food.getName());
+        }
+    }
+
+    @Override
+    public void onRecipeListRespone(Response<CompactRecipe> response) {
+        System.out.println("TOTAL RECIPES: " + response.getTotalResults());
+
+        List<CompactRecipe> recipes = response.getResults();
+        System.out.println("=========RECIPES==========");
+        for (CompactRecipe recipe: recipes) {
+            System.out.println(recipe.getName());
+        }
+    }
+
+    @Override
+    public void onFoodResponse(Food food) {
+        Log.d("FAT_SECRET", "onFoodResponse: FOOD NAME: " + food.getName());
+    }
+
+    @Override
+    public void onRecipeResponse(Recipe recipe) {
+        System.out.println("RECIPE NAME: " + recipe.getName());
+    }
+}
+
