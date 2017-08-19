f453573ca25baa51e754e8f335136b51f2ab8162
Kevin-De <kevinde@hotmail.co.uk>
2017-08-19 20:22:46 +0100

    graph updates slowly but doesnt crash now new bugs - after adding a meal and attempting to go on the same meal menu it will instead go to the quantity activity  - also when the total calories for the day after changed mid graph animation then it will continue with its initial task before updating the graph with the new values

--- app/src/main/java/com/example/kleog/fitnessapp/ViewModels/MainActivityGraphViewModel.java
+++ app/src/main/java/com/example/kleog/fitnessapp/ViewModels/MainActivityGraphViewModel.java
@@ -4,18 +4,14 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.MainThread;
 import android.util.Log;
 
-import com.example.kleog.fitnessapp.Models.DailyUserInfoModel;
 import com.example.kleog.fitnessapp.Models.UserNutritionDB;
-import com.jjoe64.graphview.series.DataPoint;
 
 import java.util.Date;
 import java.util.Objects;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Kevin on 19/08/2017.
@@ -38,26 +34,24 @@ public class MainActivityGraphViewModel extends AndroidViewModel {
         appDatabase = UserNutritionDB.getDatabase(this.getApplication());
 
 
-
         //sets the calories to be displayed as what is already in the database
 
 
     }
 
-    public CaloriesDisplayedLiveData getCaloriesDisplayed(){
+    public CaloriesDisplayedLiveData getCaloriesDisplayed() {
 
-        if(caloriesDisplayed == null){
+        if (caloriesDisplayed == null) {
 
             Double initialCalories = null;
             try {
-                initialCalories = new AsyncTask<Void, Void, Double>(){
+                initialCalories = new AsyncTask<Void, Void, Double>() {
                     @Override
                     protected Double doInBackground(Void... params) {
 
                         try {
                             return appDatabase.DailyUserInfoModel().getDate(new Date()).getTotalCalories();
-                        }
-                        catch (Exception e){
+                        } catch (Exception e) {
                             return null;
                         }
 
@@ -74,79 +68,82 @@ public class MainActivityGraphViewModel extends AndroidViewModel {
     }
 
     @SuppressLint("StaticFieldLeak")
-    public void changeInCalories(Double newCalories){
-        if (updatingCaloriesDisplayed == null){
-            updatingCaloriesDisplayed = new AsyncTask<Double, Void, Void>() {
+    public void changeInCalories(Double newCalories) {
+        if (updatingCaloriesDisplayed == null) {
 
-                @Override
-                protected Void doInBackground(Double... params) {
-
-                    //params[0] = newCalories
-
-                    Log.d(TAG, "doInBackground: calories: " + params[0] + ", old calories: " + caloriesDisplayed.getValue());
-                    //if graph is increasing
-                    if(params[0] - caloriesDisplayed.getValue() > 0.0) {
-                        while(!Objects.equals(caloriesDisplayed.getValue(), params[0])){
-                            Log.d(TAG, "doInBackground: calories displayed: " + caloriesDisplayed.getValue());
-                            caloriesDisplayed.changePostValue(caloriesDisplayed.getValue() + 1);
-
-                            try {
-                                Thread.sleep(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-
-                    }else{ //graph is decreasing
-                        while(!Objects.equals(caloriesDisplayed.getValue(), params[0])){
-                            Log.d(TAG, "doInBackground: calories displayed: " + caloriesDisplayed);
-                            caloriesDisplayed.changePostValue(caloriesDisplayed.getValue() - 1);
-                            try {
-                                Thread.sleep(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-
-                    }
-                    return null;
-                }
-            };
+            updatingCaloriesDisplayed = new GraphAnimatorAsyncTask(caloriesDisplayed);
 
             updatingCaloriesDisplayed.execute(newCalories);
-        }else{
-            //if a async task is current updating the UI
-            if(updatingCaloriesDisplayed.getStatus() == AsyncTask.Status.RUNNING){
+
+        } else {
+            //if a async task is currently updating the UI
+            if (updatingCaloriesDisplayed.getStatus() == AsyncTask.Status.RUNNING) {
                 Log.d(TAG, "changeInCalories: Async Task was running and now got cancelled");
                 updatingCaloriesDisplayed.cancel(true);
 
+                updatingCaloriesDisplayed = new GraphAnimatorAsyncTask(caloriesDisplayed);
+
                 updatingCaloriesDisplayed.execute(newCalories);
 
-            }
-            else if(updatingCaloriesDisplayed.getStatus() == AsyncTask.Status.FINISHED){
-                Log.d(TAG, "changeInCalories: Async Task was finished, now retarting");
+            } else if (updatingCaloriesDisplayed.getStatus() == AsyncTask.Status.FINISHED) {
+                Log.d(TAG, "changeInCalories: Async Task was finished, now retarting with a new async task");
+                updatingCaloriesDisplayed = new GraphAnimatorAsyncTask(caloriesDisplayed);
 
                 updatingCaloriesDisplayed.execute(newCalories);
             }
         }
     }
 
-    private static class GraphAnimatorAsyncTask extends AsyncTask<Void, Void, Void>{
+    private static class GraphAnimatorAsyncTask extends AsyncTask<Double, Void, Void> {
+
+        private CaloriesDisplayedLiveData caloriesDisplayed;
+
+        public GraphAnimatorAsyncTask(CaloriesDisplayedLiveData liveData) {
+            caloriesDisplayed = liveData;
+        }
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Void doInBackground(Double... params) {
+
+            //params[0] = newCalories
+
+            Log.d(TAG, "doInBackground: calories: " + params[0] + ", old calories: " + caloriesDisplayed.getValue());
+            //if graph is increasing
+            if (params[0] - caloriesDisplayed.getValue() > 0.0) {
+                while (!Objects.equals(caloriesDisplayed.getValue(), params[0])) {
+                    //Log.d(TAG, "doInBackground: calories displayed: " + caloriesDisplayed.getValue());
+                    caloriesDisplayed.changePostValue(caloriesDisplayed.getValue() + 1);
+
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            } else { //graph is decreasing
+                while (!Objects.equals(caloriesDisplayed.getValue(), params[0])) {
+                    //Log.d(TAG, "doInBackground: calories displayed: " + caloriesDisplayed.getValue());
+                    caloriesDisplayed.changePostValue(caloriesDisplayed.getValue() - 1);
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
             return null;
         }
     }
 
 
-
     /**
      * live data for the value to be displayed on the graph
      */
-    private static class CaloriesDisplayedLiveData extends LiveData<Double> {
+    public static class CaloriesDisplayedLiveData extends LiveData<Double> {
         private static CaloriesDisplayedLiveData sInstance;
 
         private Double caloriesDisplayed;
@@ -163,17 +160,17 @@ public class MainActivityGraphViewModel extends AndroidViewModel {
             return sInstance;
         }
 
-        public void changeValue(Double value){
+        public void changeValue(Double value) {
             caloriesDisplayed = value;
             setValue(value);
         }
 
-        public void changePostValue(Double value){
+        public void changePostValue(Double value) {
             caloriesDisplayed = value;
             postValue(value);
         }
 
-        public Double getValue(){
+        public Double getValue() {
             return caloriesDisplayed;
         }
 
